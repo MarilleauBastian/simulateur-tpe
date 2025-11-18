6a8eef1e3298d8068670fabeba300845f71f0963
MarilleauBastian <MarilleauBastian@gmail.com>
2025-11-18 09:35:19 +0100

    géré les boutons chiffres #6

--- scripts/app.js
+++ scripts/app.js
@@ -2,43 +2,74 @@
   const btnVert = document.querySelector(".bouton-tpe-vert");
   const btnRouge = document.querySelector(".bouton-tpe-rouge");
 
-  const btnAnnuler = document.querySelector(".btn-annuler");
-  const btnReussie = document.querySelector(".btn-reussie");
-  const btnEchouee = document.querySelector(".btn-echouee");
+  const btnAnnuler = document.querySelector(".btn-annulation-transaction");
+  const btnReussie = document.querySelector(".btn-reussite-transaction");
+  const btnEchouee = document.querySelector(".btn-echec-transaction");
 
   const modal = document.querySelector(".modal");
 
   const ligne1 = document.querySelector(".ecran-ligne1");
+  const ligne2 = document.querySelector(".ecran-ligne2");
 
-  /* --- Bouton VERT = ouvrir la sélection de type de transaction --- */
-  btnVert.addEventListener("click", () => {
-    modal.hidden = false;
+  const pad = document.querySelector(".tpe-boutons");
+
+  const texteInitial = document.querySelector(".ligne1");
+
+  ligne1.textContent += "20€";
+
+  const codeAttendu = "1234";
+
+  let codeSaisi = "";
+
+  function renitialiser() {
+    ligne2.textContent = "";
+    codeSaisi = "";
+  }
+
+  /* Listener TPE */
+
+  pad.addEventListener("click", (e) => {
+    const btn = e.target.closest("button");
+    if (!btn) return;
+
+    const key = btn.textContent.trim();
+    //if (ligne1 === "20€") {
+    if (/^[0-9]$/.test(key) && codeSaisi.length < 4) {
+      ligne2.textContent += "*";
+      codeSaisi += key;
+    }
+    //}
+    else if (key === "O") {
+      if (codeSaisi === codeAttendu) {
+        modal.hidden = false;
+      } else {
+        ligne1.textContent = "Code erroné";
+        renitialiser();
+      }
+    } else if (key === "X") {
+      ligne1.textContent = "Transaction annulée";
+      renitialiser();
+    }
   });
 
-  /* --- Bouton ROUGE = transaction abandonnée --- */
-  btnRouge.addEventListener("click", () => {
-    ligne1.textContent = "Transaction annulée";
-  });
+  /* Listeners du modal */
 
-  /* bouton transaction*/
+  /* --- Bouton réussite = simule une transaction écouchée et ferme le modal --- */
   btnReussie.addEventListener("click", () => {
+    modal.hidden = true;
     ligne1.textContent = "Transaction réussie";
+    renitialiser();
   });
 
+  /* --- Bouton echec = simule une transaction écouchée et ferme le modal --- */
   btnEchouee.addEventListener("click", () => {
+    modal.hidden = true;
     ligne1.textContent = "Transaction échouée";
+    renitialiser();
   });
 
-  /* --- Bouton ANNULER = fermer la sélection de type de transaction --- */
+  /* --- Bouton ANNULER = ferme le modal --- */
   btnAnnuler.addEventListener("click", () => {
-    modal.hidden = true;
-  });
-
-  btnReussie.addEventListener("click", () => {
-    modal.hidden = true;
-  });
-
-  btnEchouee.addEventListener("click", () => {
     modal.hidden = true;
   });
 })();
